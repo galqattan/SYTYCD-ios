e4d81bb9cb1123f3060fe2d98cd3ff35a2cc67e8
galqattan <g_alqattan@icloud.com>
2020-07-07 19:38:55 +0300

    travel buddy

---
+++ TravelBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelBuddy
+//
+//  Created by ghadeer alqattan on 7/7/20.
+//  Copyright © 2020 GeeCodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
